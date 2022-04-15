ea8aa37a337045bb02a2b2d3410cb5239721d14f
havaeng <havang@me.com>
2022-04-15 13:35:21 +0200

    Ändrade addItem() i ItemController till @PostMapping istället för @GetMapping

--- src/main/java/mau/project/sharepool/item/ItemController.java
+++ src/main/java/mau/project/sharepool/item/ItemController.java
@@ -2,6 +2,7 @@ package mau.project.sharepool.item;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,7 @@ public class ItemController {
         return service.getItems();
     }
 
-    @GetMapping(path = "add")
+    @PostMapping(path = "add")
     public void addItem(Item item){
         service.addItem(item);
     }
