9dddba0ee1021c5d0ea41b4be3ff8ec6ce15640b
Hugo Lindstedt <hugolindstedt@outlook.com>
2022-04-19 16:14:06 +0200

    fixed autentication

--- src/main/java/mau/project/sharepool/login/Login2.java
+++ src/main/java/mau/project/sharepool/login/Login2.java
@@ -22,25 +22,14 @@ public class Login {
             generator = "login_id_seq"
     )
     private Long id;
-    private String username;
-    private String password;
+    //private String username;
     private String email;
+    private String password;
     private Date last_logged_in;
     @JsonProperty("is_active")
     private boolean is_active;
 
     public Login() {
-    }
-
-    public Login(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public Login(Long id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
     }
 
     public Long getId() {
@@ -51,12 +40,12 @@ public class Login {
         this.id = id;
     }
 
-    public String getUsername() {
-        return username;
+    public String getEmail() {
+        return email;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {
@@ -65,14 +54,6 @@ public class Login {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public Date getLast_logged_in() {
@@ -95,9 +76,8 @@ public class Login {
     public String toString() {
         return "Login{" +
                 "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
                 ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
                 ", last_logged_in=" + last_logged_in +
                 ", is_active=" + is_active +
                 '}';
