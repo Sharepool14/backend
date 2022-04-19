7717a992df79f953dd80d702379a8ee7b164848c
Hugo Lindstedt <hugolindstedt@outlook.com>
2022-04-19 13:36:26 +0200

    First implementation Authentication/Authorization

--- src/main/java/mau/project/sharepool/SharepoolApplication.java
+++ src/main/java/mau/project/sharepool/SharepoolApplication.java
@@ -2,12 +2,20 @@ package mau.project.sharepool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SharepoolApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SharepoolApplication.class, args);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
