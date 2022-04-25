4a2d5f8eba238dcfcec66bc66bb1bb8538cf62d5
Hugo Lindstedt <hugolindstedt@outlook.com>
2022-04-25 22:09:10 +0200

    Changed so JWT tokens are added to Response body instead of headers.

--- src/main/java/mau/project/sharepool/config/AuthenticationFilter.java
+++ src/main/java/mau/project/sharepool/config/AuthenticationFilter.java
@@ -17,8 +17,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -43,20 +46,17 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
             e.printStackTrace();
         }
 
-        //String username = request.getParameter("username");
-        //String password = request.getParameter("password");
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username,password);
         return authenticationManager.authenticate(authToken);
     }
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        System.out.println("Logged in");
         User user = (User) authResult.getPrincipal();
         Algorithm algo = Algorithm.HMAC256("secret".getBytes());
         String access_token = JWT.create()
                 .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 10*60*500))
+                .withExpiresAt(new Date(System.currentTimeMillis() + 10*60*1000))
                 .withIssuer("Share Pool")
                 .withClaim("roles",user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
                 .sign(algo);
@@ -66,8 +66,11 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
                 .withExpiresAt(new Date(System.currentTimeMillis() + 10*60*2000))
                 .withIssuer("Share Pool")
                 .sign(algo);
-        response.setHeader("access_token",access_token);
-        response.setHeader("refresh_token",refresh_token);
 
+        Map<String, String>  tokens = new HashMap<>();
+        tokens.put("access_token",access_token);
+        tokens.put("refresh_token",refresh_token);
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
     }
 }
